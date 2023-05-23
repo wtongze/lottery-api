865874a88210af85c3a4346d1e92821ab5256823
Tongze Wang <wtongze@gmail.com>
2023-05-23 15:24:44 -0700

    refactor: use enum as argument

--- src/main/java/com/wtongze/lotteryapi/DrawGame.java
+++ src/main/java/com/wtongze/lotteryapi/DrawGame.java
@@ -1,20 +1,7 @@
 package com.wtongze.lotteryapi;
 
-import java.util.Arrays;
-import java.util.Optional;
-
 public enum DrawGame {
-    POWER_BALL("powerball"),
-    SUPER_LOTTO_PLUS("superlottoplus"),
-    FANTASY_5("fantasy5");
-
-    private final String label;
-
-    public static Optional<DrawGame> fromString(String val) {
-        return Arrays.stream(DrawGame.values()).filter(i -> i.label.equals(val)).findFirst();
-    }
-
-    DrawGame(String label) {
-        this.label = label;
-    }
+    POWERBALL,
+    SUPERLOTTO_PLUS,
+    FANTASY_5
 }
--- src/main/java/com/wtongze/lotteryapi/GeneratorController.java
+++ src/main/java/com/wtongze/lotteryapi/GeneratorController.java
@@ -1,13 +1,10 @@
 package com.wtongze.lotteryapi;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collection;
 import java.util.List;
@@ -21,22 +18,18 @@ public class GeneratorController {
 
     @GetMapping("/generate")
     @Operation(description = "Generate Quick Picks for DrawGame")
-    public GeneratorResponse loginTest(@RequestParam @Parameter(example = "powerball") String game,
+    public GeneratorResponse loginTest(@RequestParam DrawGame game,
                                        @RequestParam Optional<Integer> count) {
-        DrawGame drawGame = DrawGame.fromString(game).orElse(null);
-        if (drawGame == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }
         GeneratorResponse res = new GeneratorResponse();
         for (int i = 0; i < count.orElse(1); i++) {
             List<Integer> result = null;
-            switch (drawGame) {
-                case POWER_BALL -> {
+            switch (game) {
+                case POWERBALL -> {
                     List<Integer> whiteBalls = generator.intBetween(5, 69);
                     List<Integer> redBall = generator.intBetween(1, 26);
                     result = Stream.of(whiteBalls, redBall).flatMap(Collection::stream).toList();
                 }
-                case SUPER_LOTTO_PLUS -> {
+                case SUPERLOTTO_PLUS -> {
                     List<Integer> whiteBalls = generator.intBetween(5, 47);
                     List<Integer> redBall = generator.intBetween(1, 27);
                     result = Stream.of(whiteBalls, redBall).flatMap(Collection::stream).toList();
