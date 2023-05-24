bf629516a5884930724aa2b1e48618d35cd80faf
Tongze Wang <wtongze@gmail.com>
2023-05-24 00:52:13 -0700

    fix: allow access to api doc

--- src/main/java/com/wtongze/lotteryapi/SecurityConfig.java
+++ src/main/java/com/wtongze/lotteryapi/SecurityConfig.java
@@ -19,9 +19,16 @@ public class SecurityConfig {
         http
                 .authorizeExchange(auth -> auth.pathMatchers("/auth")
                         .permitAll()
+                        .pathMatchers("/swagger-ui.html")
+                        .permitAll()
+                        .pathMatchers("/webjars/swagger-ui/**")
+                        .permitAll()
+                        .pathMatchers("/v3/api-docs/**")
+                        .permitAll()
                         .anyExchange()
                         .authenticated())
-                .csrf(ServerHttpSecurity.CsrfSpec::disable);
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable);
         return http.build();
     }
 
