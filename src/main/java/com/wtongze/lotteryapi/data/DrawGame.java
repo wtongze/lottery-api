d3b7a1373b346f9006325cee442b584e8fff52a2
Tongze Wang <wtongze@gmail.com>
2023-05-31 11:25:40 -0700

    feat: add gameId for DrawGame

--- src/main/java/com/wtongze/lotteryapi/data/DrawGame.java
+++ src/main/java/com/wtongze/lotteryapi/data/DrawGame.java
@@ -1,9 +1,14 @@
 package com.wtongze.lotteryapi.data;
 
 public enum DrawGame {
-    POWERBALL,
-    SUPERLOTTO,
-    FANTASY5,
-    DAILY3,
-    DAILY4
+    POWERBALL(12),
+    SUPERLOTTO(8),
+    FANTASY5(10),
+    DAILY3(9),
+    DAILY4(14);
+
+    private final Integer gameId;
+    DrawGame(Integer gameId) {
+        this.gameId = gameId;
+    }
 }
